98782d776f12a84a893d41f8bcf6b85504a6cb7a
kelincaca <kelincacaca@gmail.com>
2026-09-19 14:21:44 +0800

    First day1

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -1,5 +1,10 @@
 #include<raylib.h>
 #include"gameMain.h"
+struct GameData
+{
+    float positionX = 100;
+    float positionY = 100;
+}gameData;
 bool initGame()
 {
     return true;
@@ -10,8 +15,13 @@ bool updateGame()
         c.r=255;
         c.g=0;
         c.b=200;
-        c.a=255;
-        DrawText("Wow!raylib!",100,100,30,BLACK);
+        c.a = 255;
+        if (IsKeyDown(KEY_A)) { gameData.positionX -= 1; }
+        if (IsKeyDown(KEY_D)) { gameData.positionX += 1; }
+        if (IsKeyDown(KEY_W)) { gameData.positionY -= 1; }
+        if (IsKeyDown(KEY_S)) { gameData.positionY += 1; }
+        //positionX,positionY,sizeX,sizeY,color
+        DrawRectangle(gameData.positionX, gameData.positionY, 50, 50, c);
     return true; 
 }
 void closeGame()
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,59 @@
 #include <raylib.h>
 #include <iostream>
-#include<imgui.h>
-#include<rlImGui.h>
-int main()
+#include <imgui.h>
+#include <rlImGui.h>
+#include <gameMain.h>
+
+int main(void)
 {
+#if PRODUCTION_BUILD==1
+    SetTraceLogLevel(LOG_NONE);
+#endif
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 450, "window name");
+    SetExitKey(KEY_NULL);
+    SetTargetFPS(240);
+
+#pragma region imgui
     rlImGuiSetup(true);
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.FontGlobalScale = 2.5;
+#pragma endregion
+    if (!initGame()) {
+        return 0;
+    }
+
     while(!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
+#pragma region imgui
         rlImGuiBegin();
-        Color c;
-        c.r = 255;
-        c.g = 0;
-        c.b = 200;
-        c.a = 255;
-        DrawText("Hello, World!", 190, 200, 20, RED);
-        ImGui::Begin("test");
-        ImGui::Text("hello");
-        ImGui::Button("button");
-        ImGui::End();
+#pragma endregion
+#pragma region imgui
+        ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+        ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+        ImGuiID dockId = ImGui::GetID("MainDockSpace");
+        ImGui::DockSpaceOverViewport(dockId, ImGui::GetMainViewport());
+        ImGui::PopStyleColor(2);
+#pragma endregion
+        if (!updateGame())
+        {
+            CloseWindow();
+        }
+#pragma region imgui
         rlImGuiEnd();
+#pragma endregion
         EndDrawing(); 
     }
+#pragma region imgui
     rlImGuiShutdown();
+#pragma endregion
     CloseWindow();
+    closeGame();
+
+#pragma region imgui
+    rlImGuiShutdown();
+#pragma endregion
     return 0;
 }
